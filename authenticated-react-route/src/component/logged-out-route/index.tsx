87cfd84835988e3c90cac6b822e4b0acc54a9ac4
Ricky_home_pc <guagin0972@gmail.com>
2020-05-23 09:43:19 +0800

    add logged in and logged out route.

---
+++ authenticated-react-route/src/component/logged-out-route/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+interface Props {
+  exact?: boolean;
+  path: string;
+  component: React.ComponentType<any>;
+}
+
+const LoggedOutRoute = ({ component: Component, ...otherProps }: Props) => (
+  <>
+    <header>Logged In header.</header>
+    <Route
+      render={() => (
+        <>
+          <Component {...otherProps} />
+        </>
+      )}
+    />
+    <footer>Logged In Footer.</footer>
+  </>
+);
+
+export default LoggedOutRoute;
--- authenticated-react-route/src/store/current/constant.ts
+++ authenticated-react-route/src/store/current/constant.ts
@@ -1,2 +1,5 @@
 export const AUTHENTICATE = 'Authenticate';
 export const UNAUTHENTICATE = 'Unauthenticate';
+
+export type AUTHENTICATE = typeof AUTHENTICATE;
+export type UNAUTHENTICATE = typeof UNAUTHENTICATE;
